37351054d71af82e9f7774b22a89ffbcf23e9a05
EduGro <73360551+EduGro@users.noreply.github.com>
2021-02-08 23:07:03 -0600

    Final

---
+++ dist/assets/scripts/main.js
@@ -0,0 +1,51 @@
+var theNews;
+var search = 'country=us&';
+var url = 'http://newsapi.org/v2/top-headlines?' +
+    search +
+    'apiKey=5dc413002f494563be38862bed15f046';
+function fetching() {
+    var req = new Request(url);
+    fetch(req)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (myJson) {
+        theNews = myJson.articles;
+    })
+        .then(function () {
+        init();
+    });
+}
+function init() {
+    const templateSource = `{{#each news}}
+    <div id="col-3">
+
+        <img src="{{urlToImage}}">
+        <h2>{{title}}</h2>
+        <p>{{description}}</p>
+        <a href="{{url}}" target="_blank">Ver más</a>
+
+    </div>
+    {{/each}}`;
+    const template = Handlebars.compile(templateSource);
+    console.log("I am working");
+    console.log(templateSource);
+    console.log(template);
+    document.getElementById('news-item').innerHTML = template({
+        news: theNews
+    });
+    console.log("I am done");
+}
+window.addEventListener('load', (event) => {
+    fetching();
+});
+document.getElementById("btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    search = document.getElementById('search').value;
+    search = search == "" ? 'country=us&' : `q=${search}&`;
+    console.log(search);
+    url = 'http://newsapi.org/v2/top-headlines?' +
+        search +
+        'apiKey=5dc413002f494563be38862bed15f046';
+    fetching();
+});
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let ts = require('gulp-typescript');
+let webserver = require('gulp-webserver');
+
+const buildPath = 'dist';
+
+gulp.task('styles', function(){
+    return gulp.src('src/**/*.scss') //origen
+        .pipe(sass()) //transform
+        .pipe(gulp.dest(buildPath + '/assets/styles')) ;//destino
+})
+
+
+gulp.task('watch:styles', gulp.series('styles', function(done){
+    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    done();
+}))
+
+
+gulp.task('scripts', function(){
+    let tsConfig = ts.createProject('tsconfig.json');
+    return tsConfig.src() //origen
+        .pipe(tsConfig()) //transform
+        .pipe(gulp.dest(buildPath + '/assets/scripts')); //destino
+})
+
+gulp.task('watch:scripts', gulp.series('scripts', function(done){
+    gulp.watch('src/**/*.ts', gulp.series('scripts'));
+    done();
+}))
+
+gulp.task('html', function(){
+    return gulp.src('src/index.html') //origen
+        .pipe(gulp.dest(buildPath)); //destino
+})
+
+
+gulp.task('watch:html', gulp.series('html', function(done){
+    gulp.watch('src/**/*.html', gulp.series('html'));
+    done();
+}))
+
+
+gulp.task('serve', function(){
+    return gulp.src(buildPath) //origen
+        .pipe(webserver({
+            open: true,
+            livereload: true
+        })) //destino
+});
+
+gulp.task('default', gulp.parallel('watch:styles', 'watch:scripts', 'watch:html', 'serve'));
